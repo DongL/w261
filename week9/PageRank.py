5dccc281ae77ede1497aebc786e8286f460ec38e
Jason Sanchez <jason.sanchez@blueowl.xyz>
2016-11-14 01:39:55 -0800

    Get distributed version to work but for an incorrect node count

--- week9/PageRank.py
+++ week9/PageRank.py
@@ -1,9 +1,13 @@
 
-from __future__ import print_function
+from __future__ import print_function, division
+import itertools
 from mrjob.job import MRJob
 from mrjob.job import MRStep
 from mrjob.protocol import JSONProtocol
 from sys import stderr
+
+def push(key, value):
+    yield (key, value)
 
 class PageRank(MRJob):
     INPUT_PROTOCOL = JSONProtocol
@@ -22,12 +26,21 @@ class PageRank(MRJob):
             exact number will be 
             updated after the first
             iteration.""")
-    
+        
     def mapper_init(self):
-        n = self.options.n_nodes
-        self.n_nodes = float(n)
+        self.values = {"****Total PR": 0.0,
+                       "***n_nodes": 0.0,
+                       "**Distribute": 0.0}
+        self.n_reducers = 2
+        
+    def mapper_final(self):
+        for key, value in self.values.items():
+            for k in range(self.n_reducers):
+                yield (k, (key, value))
     
     def mapper(self, key, lines):
+        n_reducers = self.n_reducers
+        key_hash = hash(key)%n_reducers
         # Handles special keys
         # Calculate new Total PR
         # each iteration
@@ -37,94 +50,100 @@ class PageRank(MRJob):
             # !!! This is where the special
             # hash to the same reducer code
             # will need to go.
-            yield (key, lines)
+            self.values[key] += lines
             raise StopIteration
         # Handles the first time the 
         # mapper is called. The lists
         # are converted to dictionaries 
         # with default PR values.
         if isinstance(lines, list):
-            default_PR = 1/self.n_nodes
+            n_nodes = self.options.n_nodes
+            default_PR = 1/n_nodes
             lines = {"links":lines, 
                      "PR": default_PR}
             # Also perform a node count
-            yield ("***n_nodes", 1.0)
+            self.values["***n_nodes"] += 1.0
         PR = lines["PR"]
         links = lines["links"]
         n_links = len(links)
         # Pass node onward
-        yield (key, lines)
+        yield (key_hash, (key, lines))
         # Track total PR in system
-        yield ("****Total PR", PR)
+        self.values["****Total PR"] += PR
         # If it is not a dangling node
         # distribute its PR to the 
         # other links.
         if n_links:
             PR_to_send = PR/n_links
             for link in links:
-                yield (link, PR_to_send)
+                link_hash = hash(link)%n_reducers
+                yield (link_hash, (link, PR_to_send))
         else:
-            # !!! This is also where the special
-            # hash must go.
-            yield ("**Distribute", PR)
+            self.values["**Distribute"] = PR
 
     def reducer_init(self):
         self.to_distribute = None
         self.n_nodes = None
         self.total_pr = None
     
-    def reducer(self, key, values):
-        total = 0
-        node_info = None
-        
-        for val in values:
-            if isinstance(val, float):
-                total += val
+    def reducer(self, hash_key, combo_values):
+        gen_values = itertools.groupby(combo_values, 
+                                       key=lambda x:x[0])
+        for key, values in gen_values:
+            total = 0
+            node_info = None
+
+            for key, val in values:
+                if isinstance(val, float):
+                    total += val
+                else:
+                    node_info = val
+
+            if node_info:
+                distribute = self.to_distribute or 0
+                pr = total + distribute
+                decayed_pr = .85 * pr
+                teleport_pr = .15/self.n_nodes
+                new_pr = decayed_pr + teleport_pr
+                node_info["PR"] = new_pr
+                yield (key, node_info)
+            elif key == "****Total PR":
+                self.total_pr = total
+                yield (key, total)
+            elif key == "***n_nodes":
+                self.n_nodes = total
+                yield (key, total)
+            elif key == "**Distribute":
+                extra_mass = total
+                # Because the node_count and
+                # the mass distribution are 
+                # eventually consistent, a
+                # simple correction for any early
+                # discrepancies is a good fix
+                excess_pr = self.total_pr - 1
+                weight = extra_mass - excess_pr
+                self.to_distribute = weight/self.n_nodes
             else:
-                node_info = val
-                
-        if node_info:
-            distribute = self.to_distribute or 0
-            pr = total + distribute
-            decayed_pr = .85 * pr
-            teleport_pr = .15/self.n_nodes
-            new_pr = decayed_pr + teleport_pr
-            node_info["PR"] = new_pr
-            yield (key, node_info)
-        elif key == "****Total PR":
-            self.total_pr = total
-            yield (key, total)
-        elif key == "***n_nodes":
-            self.n_nodes = total
-            yield (key, total)
-        elif key == "**Distribute":
-            extra_mass = total
-            # Because the node_count and
-            # the mass distribution are 
-            # eventually consistent, a
-            # simple correction for any early
-            # discrepancies is a good fix
-            excess_pr = self.total_pr - 1
-            weight = extra_mass - excess_pr
-            self.to_distribute = weight/self.n_nodes
-        else:
-            # The only time this should run
-            # is when dangling nodes are 
-            # discovered during the first
-            # iteration. By making them
-            # explicitly tracked, the mapper
-            # can handle them from now on.
-            yield ("**Distribute", total)
-            yield ("***n_nodes", 1.0)
-            yield (key, {"PR": total, 
-                         "links": []})
-            
+                # The only time this should run
+                # is when dangling nodes are 
+                # discovered during the first
+                # iteration. By making them
+                # explicitly tracked, the mapper
+                # can handle them from now on.
+                yield ("**Distribute", total)
+                yield ("***n_nodes", 1.0)
+                yield (key, {"PR": total, 
+                             "links": []})
+
+             
     def steps(self):
         mr_steps = [MRStep(mapper_init=self.mapper_init,
                            mapper=self.mapper,
+                           mapper_final=self.mapper_final,
                            reducer_init=self.reducer_init,
-                           reducer=self.reducer)]*100
+                           reducer=self.reducer)]*15
         return mr_steps
-        
+
+
 if __name__ == "__main__":
     PageRank.run()
