406d507fcc06b75571d4d2669c76208455d6cde1
Jason Sanchez <jason.sanchez@blueowl.xyz>
2016-11-16 03:07:22 -0800

    Add current version of HW9

---
+++ week9/WikiPageRank.py
@@ -0,0 +1,219 @@
+from __future__ import print_function, division
+import itertools
+from mrjob.job import MRJob
+from mrjob.job import MRStep
+from mrjob.protocol import JSONProtocol
+from sys import stderr
+from random import random
+import json
+
+class WikiPageRank(MRJob):    
+    def configure_options(self):
+        super(WikiPageRank, 
+              self).configure_options()
+
+        self.add_passthrough_option(
+            '--n_nodes', 
+            dest='n_nodes', 
+            type='float',
+            help="""number of nodes 
+            that have outlinks. You can
+            guess at this because the
+            exact number will be 
+            updated after the first
+            iteration.""")
+        
+        self.add_passthrough_option(
+            '--reduce.tasks', 
+            dest='reducers', 
+            type='int',
+            help="""number of reducers
+            to use. Controls the hash
+            space of the custom
+            partitioner""")
+        
+        self.add_passthrough_option(
+            '--iterations', 
+            dest='iterations', 
+            type='int',
+            help="""number of iterations
+            to perform.""")
+        
+        self.add_passthrough_option(
+            '--damping_factor', 
+            dest='d', 
+            default=.85,
+            type='float',
+            help="""Is the damping
+            factor. Must be between
+            0 and 1.""")
+        
+        self.add_passthrough_option(
+            '--smart_updating', 
+            dest='smart_updating', 
+            type='str',
+            default="False",
+            help="""Can be True or
+            False. If True, all updates
+            to the new PR will take into
+            account the value of the old
+            PR.""")
+        
+    def clean_data(self, _, lines):
+        key, value = lines.split("\t")
+        value = json.loads(value.replace("'", '"'))
+        values = value.keys()
+        yield (str(key), values)
+        
+    def mapper_init(self):
+        self.values = {"****Total PR": 0.0,
+                       "***n_nodes": 0.0,
+                       "**Distribute": 0.0}
+        self.n_reducers = self.options.reducers
+    
+    def mapper(self, key, lines):
+        n_reducers = self.n_reducers
+        key_hash = hash(key)%n_reducers
+        # Handles special keys
+        # Calculate new Total PR
+        # each iteration
+        if key in ["****Total PR"]:
+            raise StopIteration
+        if key in ["**Distribute"]:
+            self.values[key] += lines
+            raise StopIteration
+        if key in ["***n_nodes"]:
+            self.values[key] += lines
+            raise StopIteration
+        # Handles the first time the 
+        # mapper is called. The lists
+        # are converted to dictionaries 
+        # with default PR values.
+        if isinstance(lines, list):
+            n_nodes = self.options.n_nodes
+            default_PR = 1/n_nodes
+            lines = {"links":lines, 
+                     "PR": default_PR}
+        # Perform a node count each time
+        self.values["***n_nodes"] += 1.0
+        PR = lines["PR"]
+        links = lines["links"]
+        n_links = len(links)
+        # Pass node onward
+        yield (key_hash, (key, lines))
+        # Track total PR in system
+        self.values["****Total PR"] += PR
+        # If it is not a dangling node
+        # distribute its PR to the 
+        # other links.
+        if n_links:
+            PR_to_send = PR/n_links
+            for link in links:
+                link_hash = hash(link)%n_reducers
+                yield (link_hash, (link, PR_to_send))
+        else:
+            self.values["**Distribute"] = PR
+
+    def mapper_final(self):
+        for key, value in self.values.items():
+            for k in range(self.n_reducers):
+                yield (k, (key, value))
+            
+    def reducer_init(self):
+        self.d = self.options.d
+        smart = self.options.smart_updating
+        if smart == "True":
+            self.smart = True
+        elif smart == "False":
+            self.smart = False
+        else:
+            msg = """--smart_updating should 
+                       be True or False"""
+            raise Exception(msg)
+        self.to_distribute = None
+        self.n_nodes = None
+        self.total_pr = None
+
+    def reducer(self, hash_key, combo_values):
+        gen_values = itertools.groupby(combo_values, 
+                                       key=lambda x:x[0])
+        for key, values in gen_values:
+            total = 0
+            node_info = None
+
+            for key, val in values:
+                if isinstance(val, float):
+                    total += val
+                else:
+                    node_info = val
+
+            if node_info:
+                old_pr = node_info["PR"]
+                distribute = self.to_distribute or 0
+                pr = total + distribute
+                decayed_pr = self.d * pr
+                teleport_pr = (1-self.d)/self.n_nodes
+                new_pr = decayed_pr + teleport_pr
+                if self.smart:
+                    # If the new value is less than
+                    # 30% different than the old
+                    # value, set the new PR to be
+                    # 80% of the new value and 20% 
+                    # of the old value.
+                    diff = abs(new_pr - old_pr)
+                    percent_diff = diff/old_pr
+                    if percent_diff < .3:
+                        new_pr = .8*new_pr + .2*old_pr
+                if new_pr < 0:
+                    new_pr = 0
+                node_info["PR"] = new_pr
+                yield (key, node_info)
+            elif key == "****Total PR":
+                self.total_pr = total
+            elif key == "***n_nodes":
+                self.n_nodes = total
+            elif key == "**Distribute":
+                extra_mass = total
+                # Because the node_count and
+                # the mass distribution are 
+                # eventually consistent, a
+                # simple correction for any early
+                # discrepancies is a good fix
+                excess_pr = self.total_pr - 1
+                weight = extra_mass - excess_pr
+                self.to_distribute = weight/self.n_nodes
+            else:
+                # The only time this should run
+                # is when dangling nodes are 
+                # discovered during the first
+                # iteration. By making them
+                # explicitly tracked, the mapper
+                # can handle them from now on.
+                yield ("**Distribute", total)
+                yield ("***n_nodes", 1.0)
+                yield (key, {"PR": total, 
+                             "links": []})
+
+    def reducer_final(self):
+        print_info = False
+        if print_info:
+            print("Total PageRank", self.total_pr)
+        
+    def steps(self):
+        iterations = self.options.iterations
+        mr_steps = ([MRStep(mapper=self.clean_data)] 
+                    +
+                    [MRStep(
+                           mapper_init=self.mapper_init,
+                           mapper=self.mapper,
+                           mapper_final=self.mapper_final,
+                           reducer_init=self.reducer_init,
+                           reducer=self.reducer,
+                           reducer_final=self.reducer_final
+                            )]*iterations
+                   )
+        return mr_steps
+
+
+if __name__ == "__main__":
+    WikiPageRank.run()
